4871f1d7faccc4c2dc1ef842b9dbe2a329dfa5cc
Antonio Melo <antonio.melo@traycorp.com.br>
2021-03-04 09:15:12 -0300

    Scaffold de views e models do asp.net identity.

---
+++ accounts-oidc-client/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using accounts_oidc_client.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(accounts_oidc_client.Areas.Identity.IdentityHostingStartup))]
+namespace accounts_oidc_client.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
